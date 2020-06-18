ed992a3682ff5b6703fb5142e154d86fa35ab62a
saikiran <skiran252@gmail.com>
2020-06-18 14:02:48 +0530

    added user model

--- src/app.js
+++ src/app.js
@@ -4,11 +4,19 @@ const hbs       = require('hbs')
 
 const port = process.env.PORT || 3000
 const app = express()
-
+//converts response to json or else its made invalid
 app.use(express.json())
-
+//public path
 const publicDir = path.join(__dirname, '../public')
 app.use(express.static(publicDir))
+
+app.set('view engine',hbs)
+const viewspath = path.join(__dirname, '../templates/views')
+
+app.set('views',viewspath)
+const partialspath = path.join(__dirname, '../templates/partials')
+
+hbs.registerPartials(partialspath)
 
 app.get('/',(req,res)=>{
 
